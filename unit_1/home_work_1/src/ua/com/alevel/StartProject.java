cae168a14c876d4155771eed895e74562038edda
Mikhail Talancev <mikhailtalancev@gmail.com>
2024-04-01 18:16:51 +0300

    done_hw_1

---
+++ unit_1/home_work_1/src/ua/com/alevel/StartProject.java
@@ -0,0 +1,12 @@
+package ua.com.alevel;
+
+public class StartProject {
+    public static void main(String[] args) {
+        ua.com.alevel.print.Print pl = new ua.com.alevel.print.Print();
+
+
+        String msg = "Hello World";
+
+        pl.print(msg);
+    }
+}
---
+++ unit_1/home_work_1/src/ua/com/alevel/print/Print.java
@@ -0,0 +1,9 @@
+package ua.com.alevel.print;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Print {
+    public void print(String msg){
+        System.out.println(StringUtils.lowerCase(msg));
+    }
+}
