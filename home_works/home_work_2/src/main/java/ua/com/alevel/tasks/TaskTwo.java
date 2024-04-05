7614a09bdeebaf609887e001eb79cc69b4abd4c5
Mikhail Talancev <mikhailtalancev@gmail.com>
2024-04-05 17:05:39 +0300

    hw_2 done task 2

--- home_works/home_work_2/src/main/java/ua/com/alevel/tasks/TaskTwo.java
+++ home_works/home_work_2/src/main/java/ua/com/alevel/tasks/TaskTwo.java
@@ -3,9 +3,7 @@ package ua.com.alevel.tasks;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class TaskTwo {
     private String getData() throws IOException {
@@ -16,11 +14,33 @@ public class TaskTwo {
         return reader.readLine();
     }
 
-    private void print(Character a, int b){
-        System.out.println(a + " " + b);
+    private void print(int count, Character a, Integer b){
+        System.out.println(count + "." + " " + a + " " + b);
     }
 
     public void doTask() throws IOException {
         String data = getData();
+
+        char[] dataArray = data.toCharArray();
+
+        HashMap<Character, Integer> parseData = new HashMap<Character, Integer>();
+
+        for(char aChar :  dataArray){
+            if(!parseData.containsKey(aChar)){
+                parseData.put(aChar, 1);
+            } else {
+                parseData.replace(aChar, parseData.get(aChar)+1);
+            }
+        }
+
+        Set<Character> parseDataKeys = parseData.keySet();
+        int count = 1;
+        for (Character a : parseDataKeys){
+            if(a >= 'a' && a <= 'z') {print(count, a, parseData.get(a)); count++;}
+            if(a >= 'A' && a <= 'Z') {print(count, a, parseData.get(a)); count++;}
+            if(a >= 'а' && a <= 'я') {print(count, a, parseData.get(a)); count++;}
+            if(a >= 'А' && a <= 'Я') {print(count, a, parseData.get(a)); count++;}
+
+        }
     }
 }
